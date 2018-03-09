b22c577c768c121ed267caf47c1020f503c044cc
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-09 13:05:00 -0500

    Add key handling for moving with the keypad

--- input_handlers.py
+++ input_handlers.py
@@ -26,23 +26,27 @@ def handle_player_turn_keys(key):
         key_char = chr(key.c)
 
     # Movement keys
-    if key.vk == libtcod.KEY_UP or key_char == 'k':
+    if (key.vk == libtcod.KEY_UP or key_char == 'k' or
+            key.vk == libtcod.KEY_KP8):
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN or key_char == 'j':
+    elif (key.vk == libtcod.KEY_DOWN or key_char == 'j' or
+          key.vk == libtcod.KEY_KP2):
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT or key_char == 'h':
+    elif (key.vk == libtcod.KEY_LEFT or key_char == 'h' or
+          key.vk == libtcod.KEY_KP4):
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT or key_char == 'l':
+    elif (key.vk == libtcod.KEY_RIGHT or key_char == 'l' or
+          key.vk == libtcod.KEY_KP6):
         return {'move': (1, 0)}
-    elif key_char == 'y':
+    elif key_char == 'y' or key.vk == libtcod.KEY_KP7:
         return {'move': (-1, -1)}
-    elif key_char == 'u':
+    elif key_char == 'u' or key.vk == libtcod.KEY_KP9:
         return {'move': (1, -1)}
-    elif key_char == 'b':
+    elif key_char == 'b' or key.vk == libtcod.KEY_KP1:
         return {'move': (-1, 1)}
-    elif key_char == 'n':
+    elif key_char == 'n' or key.vk == libtcod.KEY_KP3:
         return {'move': (1, 1)}
-    elif key_char == 'z':
+    elif key_char == 'z' or key.vk == libtcod.KEY_KP5:
         return {'wait': True}
 
     if key_char == 'g':
@@ -160,4 +164,3 @@ def handle_main_menu(key):
         return {'exit': True}
 
     return {}
-
