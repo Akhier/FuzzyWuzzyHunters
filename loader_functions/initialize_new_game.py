41d12198d18e2d13b50ea45349ada84f3af10e59
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-09 16:03:01 -0500

    Messing with colors of wall and ground

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -83,8 +83,8 @@ def get_constants():
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150),
-        'light_wall': libtcod.Color(130, 110, 50),
-        'light_ground': libtcod.Color(200, 180, 50)
+        'light_wall': libtcod.Color(139, 69, 19),
+        'light_ground': libtcod.Color(255, 255, 255)
     }
 
     constants = {
