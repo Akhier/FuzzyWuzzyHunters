a3a88e06a983c6ab656514362aa7ea3b0111ef3d
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-09 13:42:48 -0500

    Add methods for Naked Mole Rat(Queen) and Sphynx monsters

--- map_objects/monsters.py
+++ map_objects/monsters.py
@@ -8,9 +8,15 @@ from entity import Entity
 from render_functions import RenderOrder
 
 
+def calculate_xp(hp, defense, power):
+    if defense < 1:
+        defense = 1
+    return hp * defense + power * power
+
+
 def create_orc(x, y):
     fighter_component = Fighter(
-        hp=randint(18, 22), defense=0, power=4, xp=35)
+        hp=20, defense=0, power=4, xp=35)
     ai_component = BasicMonster()
 
     return Entity(x, y, 'o', libtcod.desaturated_green,
@@ -22,11 +28,56 @@ def create_orc(x, y):
 
 def create_troll(x, y):
     fighter_component = Fighter(
-        hp=30, defense=2, power=randint(7, 9), xp=100)
+        hp=30, defense=2, power=8, xp=100)
     ai_component = BasicMonster()
 
     return Entity(x, y, 'T', libtcod.darker_green,
                   'Troll', blocks=True,
+                  render_order=RenderOrder.ACTOR,
+                  fighter=fighter_component,
+                  ai=ai_component)
+
+
+def create_naked_mole_rat(x, y):
+    hp = randint(3, 6)
+    defense = 0
+    power = 1
+    xp = calculate_xp(hp, defense, power)
+    fighter_component = Fighter(hp, defense, power, xp)
+    ai_component = BasicMonster()
+
+    return Entity(x, y, 'n', libtcod.sepia_light,
+                  'Naked Mole Rat', blocks=True,
+                  render_order=RenderOrder.ACTOR,
+                  fighter=fighter_component,
+                  ai=ai_component)
+
+
+def create_naked_mole_rat_queen(x, y):
+    hp = randint(30, 60)
+    defense = 2
+    power = 3
+    xp = calculate_xp(hp, defense, power)
+    fighter_component = Fighter(hp, defense, power, xp)
+    ai_component = BasicMonster()
+
+    return Entity(x, y, 'n', libtcod.sepia,
+                  'Naked Mole Rat Queen', blocks=True,
+                  render_order=RenderOrder.ACTOR,
+                  fighter=fighter_component,
+                  ai=ai_component)
+
+
+def create_sphynx(x, y):
+    hp = 20
+    defense = 0
+    power = randint(3, 5)
+    xp = calculate_xp(hp, defense, power)
+    fighter_component = Fighter(hp, defense, power, xp)
+    ai_component = BasicMonster()
+
+    return Entity(x, y, 'c', libtcod.sepia_lighter,
+                  'Sphynx', blocks=True,
                   render_order=RenderOrder.ACTOR,
                   fighter=fighter_component,
                   ai=ai_component)
