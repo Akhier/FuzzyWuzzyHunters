9ea9d55ad2d208433b400b0ae9a93c2d944000cc
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2018-03-10 02:03:36 -0500

    Add dodge chance for future use

--- components/fighter.py
+++ components/fighter.py
@@ -5,12 +5,13 @@ from game_messages import Message
 
 
 class Fighter:
-    def __init__(self, hp, defense, power, xp=0):
+    def __init__(self, hp, defense, power, xp=0, dodge_chance=0):
         self.base_max_hp = hp
         self.hp = hp
         self.base_defense = defense
         self.base_power = power
         self.xp = xp
+        self.dodge_chance = dodge_chance
 
     @property
     def max_hp(self):
@@ -57,29 +58,31 @@ class Fighter:
 
     def attack(self, target):
         results = []
-        chance = randint(1, 100)
-        if chance <= 25:
-            power = self.power - 1
-            if power < 1:
-                power = 1
-        elif chance <= 95:
-            power = self.power
-        else:
-            power = self.power + 1
-        damage = power - target.fighter.defense
+        accuracy = randint(1, 100)
+        if accuracy > target.dodge_chance:
+            chance = randint(1, 100)
+            if chance <= 25:
+                power = self.power - 1
+                if power < 1:
+                    power = 1
+            elif chance <= 95:
+                power = self.power
+            else:
+                power = self.power + 1
+            damage = power - target.fighter.defense
 
-        if damage > 0:
-            results.append(
-                {'message': Message(
-                    '{0} attacks {1} for {2} hit points.'.format(
-                        self.owner.name.capitalize(), target.name,
-                        str(damage)), libtcod.white)})
-            results.extend(target.fighter.take_damage(damage))
-        else:
-            results.append(
-                {'message': Message(
-                    '{0} attacks {1} but does no damage'.format(
-                        self.owner.name.capitalize(),
-                        target.name), libtcod.white)})
+            if damage > 0:
+                results.append(
+                    {'message': Message(
+                        '{0} attacks {1} for {2} hit points.'.format(
+                            self.owner.name.capitalize(), target.name,
+                            str(damage)), libtcod.white)})
+                results.extend(target.fighter.take_damage(damage))
+            else:
+                results.append(
+                    {'message': Message(
+                        '{0} attacks {1} but does no damage'.format(
+                            self.owner.name.capitalize(),
+                            target.name), libtcod.white)})
 
         return results
